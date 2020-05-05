cb0774c30af85786708c7730918f8d02a525e688
AndrewFish01 <43876174+AndrewFish01@users.noreply.github.com>
2020-05-05 13:38:56 +0700

    Add lab work

---
+++ Perl.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+open(INPUT_FILE, "< access.log");
+%ip_list = ();
+while ($line = <INPUT_FILE>) {
+    if ($line =~ m/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
+        if (exists $ip_list{$1}) {
+            $ip_list{$1}++;
+        }
+        else {
+            $ip_list{$1} = 1;
+        }
+    }
+}
+@sorted_ip_list = sort {$sorted_ip_list{$a} cmp $sorted_ip_list{$b}} keys %sorted_ip_list ;
+for (my $i=0; $i < 10; $i++) {
+    print $sorted_ip_list[$i], "\n";
+}	
